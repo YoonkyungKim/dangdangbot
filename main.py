ae4060ca1eb2caf5316a123d65f8ee893c962b86
Yoonkyung Kim <yoonkimove@gmail.com>
2018-08-07 16:10:01 +0900

    Interwork Slack

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-print("Hello World!")
-print("This is a clone of Yoonkimove's bot.")
-print("Let's eat lunch!")
+from rtmbot import RtmBot
+from rtmbot.core import Plugin
+import random
+from secret import SLACK_TOKEN
+
+class HelloPlugin(Plugin):
+    def process_message(self, data):
+        results = [' 🐕  간다 ', '왜 ❓', '🐶 🗯 🗯 🗯' , '나 고양인데? 🐱' ]
+
+        if "댕댕" in data["text"]:
+            num = random.randrange(0,3)
+            self.outputs.append([data["channel"], results[num]])
+
+config = {
+    "SLACK_TOKEN": SLACK_TOKEN,
+    "ACTIVE_PLUGINS": ["main.HelloPlugin"]
+}
+bot = RtmBot(config)
+bot.start()
+
+
+print("As soon as I have got flying to perfection, I have got a scheme about a steam engine.")
+print("Let's eat Dinner!")
 print("This is a Quotesbot.")
